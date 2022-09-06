185a38d07bd7943cc35663cec7f7e7d17650c9b6
lishuangling <595237147@qq.com>
2022-09-06 15:15:39 +0800

    vue项目初始化

---
+++ vueMinShop/vue_min_shop/src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import pers from "vuex-persistedstate";
+
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  plugins:[pers()],
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
