29ba15c962b467afd60171f8354743a455937b64
lishuangling <595237147@qq.com>
2022-09-09 11:45:09 +0800

    sl-login

--- vueMinShop/vue_min_shop/src/views/LoginView/LoginView.vue
+++ vueMinShop/vue_min_shop/src/views/LoginView/LoginView.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
-    <i class="el-icon-arrow-left" @click="goHome"></i>
+    <el-row>
+      <i class="el-icon-arrow-left" @click="goHome"></i>
+    </el-row>
     <el-row><h1>Mint外卖</h1></el-row>
     <el-row type="flex" justify="center" align="middle">
       <el-col :span="20">
         <el-tabs>
           <el-tab-pane label="短信登录">
-            <el-form>
+            <el-form :model="ruleForm" :rules="rules">
               <el-form-item>
-                <el-input type="text" placeholder="手机号" v-model="phone" />
+                <el-input
+                  type="text"
+                  placeholder="手机号"
+                  v-model="ruleForm.phone"
+                />
               </el-form-item>
               <el-form-item>
                 <el-col :span="14">
-                  <el-input v-model="code" placeholder="验证码" />
+                  <el-input v-model="ruleForm.code" placeholder="验证码" />
                 </el-col>
-                <el-col :span="1">&nbsp; </el-col>
+                <el-col :span="1">&nbsp;</el-col>
                 <el-col :span="9">
                   <el-button @click="handleCaptcha">获取验证码</el-button>
                 </el-col>
@@ -24,29 +30,33 @@
               </el-form-item>
               <el-form-item>
                 <el-link :underline="false" href="https://www.ele.me/"
-                  >关于我们</el-link
-                >
+                  >关于我们
+                </el-link>
               </el-form-item>
             </el-form>
           </el-tab-pane>
           <el-tab-pane label="密码登录">
-            <el-form>
+            <el-form :model="ruleForm" :rules="rules">
               <el-form-item>
-                <el-input placeholder="手机/邮箱/用户名" v-model="name" />
+                <el-input
+                  placeholder="手机/邮箱/用户名"
+                  v-model="ruleForm.name"
+                />
               </el-form-item>
               <el-form-item>
-                <el-input placeholder="密码" v-model="pwd" show-password />
-              </el-form-item>
-              <el-form-item>
-                <el-input placeholder="验证码" v-model="captcha" />
+                <el-input
+                  placeholder="密码"
+                  v-model="ruleForm.pwd"
+                  show-password
+                />
               </el-form-item>
               <el-form-item>
                 <el-button type="primary" @click="loginPwd">登录</el-button>
               </el-form-item>
               <el-form-item>
                 <el-link :underline="false" href="https://www.ele.me/"
-                  >关于我们</el-link
-                >
+                  >关于我们
+                </el-link>
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -57,19 +67,40 @@
 </template>
 
 <script>
+import $http from "@/api/axios";
 export default {
   name: "LoginView",
   data() {
     return {
-      name: "",
-      pwd: "",
-      captcha: "",
-      phone: "",
-      code: "",
+      ruleForm: {
+        name: "",
+        pwd: "",
+        phone: "",
+        code: "",
+      },
+      rules: {
+        name: [
+          { require: true, message: "请输入用户名", trigger: "blur" },
+          { min: 2, max: 6, message: "用户名长度2至6个字符", trigger: "blur" },
+        ],
+        pwd: [
+          { require: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 12, message: "密码长度6至12位", trigger: "blur" },
+        ],
+        phone: [
+          { require: true, message: "请输入手机号码", trigger: "blur" },
+          { min: 11, max: 11, trigger: "blur" },
+        ],
+        code: [
+          { require: true, message: "请输入验证码", trigger: "blur" },
+          { min: 4, max: 4, trigger: "blur" },
+        ],
+      },
     };
   },
   methods: {
     handleCaptcha() {
+      $http("/sendcode", { phone: this.ruleForm.phone }, "GET");
       console.log("获取验证码");
     },
     loginCode() {
@@ -79,19 +110,25 @@ export default {
       console.log("用户密码方式登录");
     },
     goHome() {
-      console.log("跳转到首页!");
+      this.$router.push("/home");
     },
   },
 };
 </script>
 
 <style scoped>
+.el-icon-arrow-left {
+  margin: 10px;
+}
+
 i {
   float: left;
 }
+
 .el-form-item {
   margin-bottom: 10px;
 }
+
 .el-button {
   width: 100%;
 }
